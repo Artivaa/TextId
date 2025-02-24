2b39de7663fb184cc1614bef81e72f6428e69f90
Artyom <artiom.vaulin2014@yandex.ru>
2025-02-24 19:30:21 +0500

    Поменял стиль

--- Main.cpp
+++ Main.cpp
@@ -2,41 +2,48 @@
 #include <string>
 #include <sstream>
 #include <cctype>
-#include <algorithm> // Для std::all_of
+#include <algorithm>
 
+using namespace std;
 
-int main() {
+int main()
+{
     setlocale(LC_ALL, "Russian");
 
-    std::string s;
-    std::cout << "Введите строку: "; 
-    std::getline(std::cin, s); 
+    string s;
+    cout << "Введите строку: ";
+    getline(cin, s);
 
-    std::stringstream ss(s);
-    std::string word;
-    std::string result;
+    stringstream ss(s);
+    string word;
+    string result;
 
-    while (ss >> word) {
-        // Более идиоматичная проверка, что слово состоит только из цифр
-        bool isDigitWord = std::all_of(word.begin(), word.end(), ::isdigit); // ::isdigit из <cctype>
+    while (ss >> word)
+    {
+        bool isDigitWord = all_of(word.begin(), word.end(), ::isdigit);
 
-        if (!isDigitWord) {
-            std::string identifier;
-            bool lastCharWasUnderscore = false; // Флаг для предотвращения дублирования '_'
+        if (!isDigitWord)
+        {
+            string identifier;
+            bool lastCharWasUnderscore = false;
 
-            // Добавляем '_' в начале, если первый символ не подходит для идентификатора
-            if (!std::isalpha(word[0]) && word[0] != '_') {
+            if (!isalpha(word[0]) && word[0] != '_')
+            {
                 identifier += '_';
-                lastCharWasUnderscore = true; // После добавления '_' считаем, что последний символ был '_'
+                lastCharWasUnderscore = true;
             }
 
-            for (char c : word) {
-                if (std::isalnum(c) || c == '_') {
+            for (char c : word)
+            {
+                if (isalnum(c) || c == '_')
+                {
                     identifier += c;
-                    lastCharWasUnderscore = false; // Допустимый символ, значит, последний '_' был давно
+                    lastCharWasUnderscore = false;
                 }
-                else {
-                    if (!lastCharWasUnderscore) { // Добавляем '_' только если предыдущий символ не был '_'
+                else
+                {
+                    if (!lastCharWasUnderscore)
+                    {
                         identifier += '_';
                         lastCharWasUnderscore = true;
                     }
@@ -46,14 +53,16 @@ int main() {
         }
     }
 
-    if (!result.empty()) {
-        result.back() = '.'; // Заменяем последний пробел на точку
+    if (!result.empty())
+    {
+        result.back() = '.';
     }
-    else {
-        result = "."; // Если result пустой, то просто "."
+    else
+    {
+        result = ".";
     }
 
-    std::cout << "Результат: " << result << std::endl;
+    cout << "Результат: " << result << endl;
 
     return 0;
 }
